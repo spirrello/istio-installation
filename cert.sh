c02137459aaefd169f65c2a2ffed9f9af8b05008
Stefano Pirrello <spirrello.com>
2020-04-02 18:00:41 -0400

    added step for deleting ingress istio pod

--- cert.sh
+++ cert.sh
@@ -14,6 +14,8 @@ KEY_CHECK=$(openssl rsa -modulus -noout -in $KEY | sha256sum | awk '{print $1}')
 if [[ $CERT_CHECK == $KEY_CHECK ]]; then
   echo "Certificate is valid, creating K8s secret now..."
   kubectl create secret tls $K8S_SECRET --cert=$CERT --key=$KEY -n $NAMESPACE
+  echo "Deleting istio-ingressgateway pods to mount certs"
+  kubectl delete pods -n istio-system -l app=istio-ingressgateway
 else
   echo "ERROR OCCURRED WHILE GENERATING CERTIFICATE"
   echo "CERT_CHECK: " $CERT_CHECK
