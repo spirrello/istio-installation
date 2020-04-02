af6a76fbca1db8845de39bda555e6aced7ab256f
Stefano Pirrello <spirrello.com>
2020-04-02 15:14:01 -0400

    fixed variables

--- cert.sh
+++ cert.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 K8S_SECRET="istio-ingressgateway-cert"
+NAMESPACE="istio-system"
 
 CERT=$1
 KEY=$2
@@ -9,9 +10,10 @@ KEY=$2
 CERT_CHECK=$(openssl x509 -modulus -noout -in $CERT  | sha256sum | awk '{print $1}')
 KEY_CHECK=$(openssl rsa -modulus -noout -in $KEY | sha256sum | awk '{print $1}')
 
-if [[ $CERT_CHECK == $CERT_KEY_CHECK ]]; then
+
+if [[ $CERT_CHECK == $KEY_CHECK ]]; then
   echo "Certificate is valid, creating K8s secret now..."
-  kubectl create secret tls $K8S_SECRET --cert=$CERT --key=$KEY -n istio-system
+  kubectl create secret tls $K8S_SECRET --cert=$CERT --key=$KEY -n $NAMESPACE
 else
   echo "ERROR OCCURRED WHILE GENERATING CERTIFICATE"
   echo "CERT_CHECK: " $CERT_CHECK
