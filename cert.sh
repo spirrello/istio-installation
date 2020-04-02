0018db291f6e4bcb899ad37624015a8d9f0f1d72
Stefano Pirrello <spirrello.com>
2020-04-02 15:06:25 -0400

    fixed openssl commands for verification

--- cert.sh
+++ cert.sh
@@ -2,18 +2,18 @@
 
 K8S_SECRET="istio-ingressgateway-cert"
 
-CERT=$2
-CERT_KEY=$3
+CERT=$1
+KEY=$2
 
 # check if the certs match
-CERT_CHECK=$(openssl pkey -in $CERT_KEY -pubout -outform pem | sha256sum)
-CERT=$(openssl x509 -in $CERT -pubkey -noout -outform pem | sha256sum)
+CERT_CHECK=$(openssl x509 -modulus -noout -in $CERT  | sha256sum)
+KEY_CHECK=$(openssl rsa -modulus -noout -in $KEY | sha256sum)
 
-
-# Check if certificate is valid
-if [[ $CERT_CHECK == $CERT ]]; then
+if [[ $CERT_CHECK == $CERT_KEY_CHECK ]]; then
   echo "Certificate is valid, creating K8s secret now..."
-  kubectl create secret tls $K8S_SECRET --cert=$CERT_NAME --key=$CERT_KEY -n istio-system
+  kubectl create secret tls $K8S_SECRET --cert=$CERT --key=$KEY -n istio-system
 else
   echo "ERROR OCCURRED WHILE GENERATING CERTIFICATE"
+  echo "CERT_CHECK: " $CERT_CHECK
+  echo "KEY_CHECK: " $KEY_CHECK
 fi
